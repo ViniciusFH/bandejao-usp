fd0b8236b55dde4e1ef99b075126fef34f3a3fc5
schimidtpedro <60239222+schimidtpedro@users.noreply.github.com>
2020-01-31 16:33:10 -0300

    Update createTweet.js
    let de_da = restaurant == 'Quimica' ? `da` : `do`;

--- createTweet.js
+++ createTweet.js
@@ -1,15 +1,17 @@
 module.exports = function(arrayCardapio, restaurant){
-
+	
 	const meal = (new Date().getHours() < 12) ? 'almoço' : 'jantar';
-
-	arrayCardapio.forEach((item, index) => arrayCardapio[index] = '· ' + item)
-
-	let header = `Hoje no ${restaurant} tem:\n`
-
-	let tweet = header + arrayCardapio.join('\n')
-
+	
+	arrayCardapio.forEach((item, index) => arrayCardapio[index] = '· ' + item);
+	
+	let de_da = restaurant == 'Quimica' ? `da` : `do`;
+	
+	let header = `Hoje no ${meal} ${de_da} ${restaurant} tem:\n`;
+	
+	let tweet = header + arrayCardapio.join('\n');
+	
 	return tweet;
-
+	
 	postTweet(restaurants['Central'].cardapio.join('\n· '))
 
 }
